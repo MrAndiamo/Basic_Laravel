caf655bee3feeda6984b15f8cfa99fc9d36c0cff
Tim van den Dries <decks16@hotmail.com>
2020-04-05 22:57:08 +0200

    added my name...

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -6,18 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Title --}}
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    {{-- Favivcon for all devices --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-
+    <title>{{ config('app.name', 'Tim van den Dries - Basic Laravel setup') }}</title>
 
     {{-- Fonts --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
