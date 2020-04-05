55e6ac145b668d60c5bca8f88c16ee7b44d9e7ba
Tim van den Dries <decks16@hotmail.com>
2020-04-05 22:42:58 +0200

    first commit for basic laravel website

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="card">
+    <div class="card-header">
+        <div class="row">
+            <div class="col-12">
+                test-titel
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body">
+
+        test-body
+
+    </div>
+
+
+</div>
+
+@endsection
